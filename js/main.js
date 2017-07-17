604e9f3e9b06cd698665119b23b2380ea673f3ed
Pranoy Dutta <pranoydll@gmail.com>
2017-07-16 20:09:17 -0500

    a basic working prototype

--- js/main.js
+++ js/main.js
@@ -2,14 +2,18 @@
 const WIDTH = 800
 const HEIGHT = 600
 
+const arrows = ['←', '↓', '↑', '→']
+
 var canvas = document.getElementById('game')
 var ctx = canvas.getContext('2d')
 
-// 0 - up
+// 0 - left
 // 1 - down
-// 2 - left
+// 2 - up
 // 3 - right
 var pressed = [false, false, false, false]
+
+var notes = new NoteList()
 
 // the inital function
 function init() {
@@ -32,6 +36,58 @@ function init() {
     }
 }
 
+function NoteList() {
+    this.stack = []
+}
+
+NoteList.prototype.addNote = function() {
+    var note = new Note(Math.floor(Math.random() * 4))
+    this.stack.push(note)
+}
+
+NoteList.prototype.update = function() {
+    for(var i = 0; i < this.stack.length; i++) {
+        if(!(this.stack[i].y < 0)) {
+            this.stack[i].update(pressed)
+        }
+    }
+}
+
+NoteList.prototype.draw = function() {
+    for(var i = 0; i < this.stack.length; i++) {
+        if(!(this.stack[i].y < 0)) {
+            this.stack[i].draw()
+        }
+    }
+}
+
+// note object
+function Note(type) {
+    this.type = type
+    this.hit = false
+    this.y = 600
+}
+
+Note.prototype.check = function() {
+    return (this.y < 130) && (this.y > 0)
+}
+
+Note.prototype.draw = function() {
+    if(!this.hit) {
+        ctx.font = '85px Source Code Pro'
+        ctx.fillStyle = '#fff'
+        ctx.fillText(arrows[this.type], WIDTH / 4 * (this.type + 1) - 100, this.y)
+    }
+}
+
+Note.prototype.update = function(pressed) {
+    this.y -= 2
+
+    if(pressed[this.type] && this.check()) {
+        this.hit = true
+    }
+}
+
 // the basic game loop
 function loop() {
     clear()
@@ -42,11 +98,19 @@ function loop() {
 // clears canvas to black
 function clear() {
     ctx.fillStyle = '#000'
-    ctx.fillRect(0, 0, 1024, 768)
+    ctx.fillRect(0, 0, WIDTH, HEIGHT)
 }
 
 // updates game logic
+var t = 0
 function update() {
+    t += 1
+    if(t % 80 == 0) {
+        t = 0
+        notes.addNote()
+    }
+
+    notes.update()
 }
 
 // key event handling
@@ -57,13 +121,18 @@ document.body.addEventListener('keydown', function(e) {
     toggle(e, true)
 })
 
+// for debugging
+document.body.addEventListener('click', function(e) {
+    console.log('x: ' + e.clientX + ' y: ' + e.clientY)
+})
+
 // sets keys in pressed array
 function toggle(e, bool) {
     switch(e.key) {
     case 'W':
     case 'w':
     case 'ArrowUp':
-        pressed[0] = bool
+        pressed[2] = bool
         e.preventDefault()
         break
     case 'S':
@@ -75,7 +144,7 @@ function toggle(e, bool) {
     case 'A':
     case 'a':
     case 'ArrowLeft':
-        pressed[2] = bool
+        pressed[0] = bool
         e.preventDefault()
         break
     case 'D':
@@ -90,13 +159,33 @@ function toggle(e, bool) {
 // draws all graphics to the screen
 function draw() {
     drawArrows()
+    drawLine()
+    notes.draw()
 }
 
+const HIT_COLOR = '#f00'
 function drawArrows() {
-    ctx.font = '55px Source Code Pro'
+    ctx.font = '85px Source Code Pro'
     ctx.fillStyle = '#fff'
-    ctx.fillText('←', WIDTH / 4 - 100, 40)
-    ctx.fillText('↓', WIDTH / 4 * 2 - 100, 40)
-    ctx.fillText('↑', WIDTH / 4 * 3 - 100, 40)
-    ctx.fillText('→', WIDTH - 100, 40)
+
+    if(pressed[0]) ctx.fillStyle = HIT_COLOR
+    ctx.fillText(arrows[0], WIDTH / 4 - 100, 70)
+
+    if(pressed[1]) ctx.fillStyle = HIT_COLOR
+    else ctx.fillStyle = '#fff'
+    ctx.fillText(arrows[1], WIDTH / 4 * 2 - 100, 70)
+
+    if(pressed[2]) ctx.fillStyle = HIT_COLOR
+    else ctx.fillStyle = '#fff'
+    ctx.fillText(arrows[2], WIDTH / 4 * 3 - 100, 70)
+
+    if(pressed[3]) ctx.fillStyle = HIT_COLOR
+    else ctx.fillStyle = '#fff'
+    ctx.fillText(arrows[3], WIDTH - 100, 70)
+}
+
+function drawLine() {
+    ctx.fillStyle = '#add8e6' // light blue
+    ctx.fillRect(0, 100, WIDTH, 1)
+    ctx.fillRect(0, 0, WIDTH, 1)
 }
